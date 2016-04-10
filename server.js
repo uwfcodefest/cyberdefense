7064c682d1321c70f7c7b76c199cfa4a09ce01ef
Ben Avellone <benjamin.avellone@gmail.com>
2016-04-10 01:37:53 -0500

    Fixed app not dying on SIGINT.

--- server.js
+++ server.js
@@ -31,6 +31,20 @@ function onListen() {
 	debug(`${chalk.green(config.app.title)} listening on port ${chalk.green(config.port)} in ${chalk.green(process.env.NODE_ENV || 'development')} mode`);
 }
 
+function shutdown() {
+	debug("Received kill signal, shutting down gracefully.");
+	app.close(function () {
+		debug("Closed out remaining connections.");
+		process.exit()
+	});
+	
+	// if after 
+	setTimeout(function () {
+		debug("Could not close connections in time, forcefully shutting down");
+		process.exit()
+	}, 10 * 1000);
+}
+
 process.on('uncaughtException', function(err) {
 	console.error(chalk.red('Caught unhandled exception!'));
 	console.error(err);
@@ -43,16 +57,19 @@ process.on('uncaughtException', function(err) {
 
 process.on('SIGINT', () => {
 	debug('Got SIGINT');
+	process.exit(0);
 });
 
 // SIGUSR2 is used by nodemon to restart the app, only bind in production
 if (process.env.NODE_ENV == 'production')
 	process.on('SIGUSR2', () => {
-		debug('Got SIGUSR2');
-	})
+		debug('Got SIGUSR2, shutting down gracefully!');
+		shutdown();
+	});
 
 process.on('SIGPIPE', () => {
 	debug('Got SIGPIPE');
 
 	console.log(util.inspect(process.memoryUsage()));
-})
+});
+
